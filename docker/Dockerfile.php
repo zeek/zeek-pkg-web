55d309130e53732d2c73eeee2d3010e844abfb73
Tim Wojtulewicz <tim@corelight.com>
2025-02-03 13:18:45 -0700

    Add cron to php image to run the update job

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -3,8 +3,13 @@ WORKDIR /var/www/html
 
 RUN apt update -y
 RUN DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+    cron \
+    git \
     libicu-dev \
     libzip-dev \
+    procps \
+    python3-pip \
+    python3-setuptools \
     unzip \
     vim
 RUN apt purge -y --auto-remove
@@ -15,6 +20,10 @@ RUN docker-php-ext-install \
     pdo \
     pdo_mysql \
     zip
+
+# Install an initial version of zkg. This gets updated by cron
+# every night before updating the packages list.
+RUN pip3 install GitPython semantic-version zkg
 
 # We could use the composer image directly here but using the php
 # one guarantees we have the same version of php installed. Instead
@@ -40,7 +49,22 @@ RUN composer install
 COPY bropkg .
 RUN composer dumpautoload --optimize
 
+# Build a final image from the above parts.
 FROM base AS final
 
 COPY --from=build --chown=www-data:www-data /var/www/html /var/www/html
 COPY --chown=www-data:www-data --chmod=640 secrets/.env /var/www/html/config/.env
+
+# zeek-package-ci is used by the cronjob to sanity check zeek packages. On the image,
+# it's stored in /usr/local/bin/bro-package-ci. We explicitly pin to version 0.4.0
+# which is the version the existing live site is using. The version on 'master' has
+# some problems with the dns_resolution check over-matching.
+RUN python3 -m pip install 'bro-package-ci@git+https://github.com/zeek/zeek-package-ci@1117e24fd80f03167ca36749bf5a246a02d86178'
+
+COPY --chmod=755 cronjob/bro-pkg-web-updater.php /usr/local/sbin
+COPY --chmod=755 cronjob/bro-pkg-web-cron.sh /etc/cron.daily
+
+# Override the existing entrypoint script so that cron can start up too.
+COPY --chmod=700 docker/php-entrypoint.sh /
+ENTRYPOINT ["/php-entrypoint.sh"]
+CMD ["php-fpm"]
