3607dc58aa644a40d8063c5fd7ef33250956c12d
Tim Wojtulewicz <tim@corelight.com>
2025-07-23 14:05:53 -0700

    Fix Paginator->counter() usage in templates
    Paginator::counter() in CakePHP 3.x takes an array of options, including
    the format. In CakePHP 4.x and later, this changed to take the format as
    a string argument first, followed by an array of the rest of the
    options.

--- bropkg/templates/Tags/index.php
+++ bropkg/templates/Tags/index.php
@@ -28,6 +28,6 @@
             <?= $this->Paginator->next() ?>
             <?= $this->Paginator->last() ?>
         </ul>
-        <p><?= $this->Paginator->counter(['format' => __('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')]) ?></p>
+        <p><?= $this->Paginator->counter('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total') ?></p>
     </div>
 </div>
