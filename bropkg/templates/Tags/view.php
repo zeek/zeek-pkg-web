e830c5f83334d9bf8fe32a70a6c30aec3580c575
Tim Wojtulewicz <tim@corelight.com>
2025-07-23 14:05:53 -0700

    Fix Paginator->counter() usage in templates

--- bropkg/templates/Tags/view.php
+++ bropkg/templates/Tags/view.php
@@ -37,6 +37,6 @@
             <?= $this->Paginator->next(__('next') . ' >') ?>
             <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
-        <p><?= $this->Paginator->counter(['format' => __('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')]) ?></p>
+        <p><?= $this->Paginator->counter('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total') ?></p>
     </div>
 </div>
