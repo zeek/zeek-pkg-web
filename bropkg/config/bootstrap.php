e8f2c779857516cd5d218ad65adde4ea337c16e1
Tim Wojtulewicz <tim@corelight.com>
2025-07-23 13:56:41 -0700

    Fix handling of errors and exceptions so they get logged properly

--- bropkg/config/bootstrap.php
+++ bropkg/config/bootstrap.php
@@ -113,12 +113,8 @@ ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
 /*
  * Register application error and exception handlers.
  */
-$isCli = PHP_SAPI === 'cli';
-if ($isCli) {
-    (new ErrorTrap(Configure::read('Error')))->register();
-} else {
-    (new ExceptionTrap(Configure::read('Error')))->register();
-}
+(new ErrorTrap(Configure::read('Error')))->register();
+(new ExceptionTrap(Configure::read('Error')))->register();
 
 /*
  * Include the CLI bootstrap overrides.
